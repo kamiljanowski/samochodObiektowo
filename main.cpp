38a48bf612c92a45b16381afa338477c67dd4db3
kamiljanowski <kamil.janowski.pl@gmail.com>
2022-10-31 19:08:23 +0100

    add all files

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "samochod.h"
+
+using namespace std;
+
+int main()
+{
+    Samochod s1;
+    s1.dodaj_samochod();
+    s1.wyswietl_samochod();
+
+    Samochod s2;
+    s2.dodaj_samochod();
+    s2.wyswietl_samochod();
+
+    Samochod s3;
+    s3.dodaj_samochod();
+    s3.wyswietl_samochod();
+
+    return 0;
+}
